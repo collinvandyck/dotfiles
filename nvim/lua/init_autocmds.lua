85d4b796806e5bb7c8d0a4c3996a5afc49455e55
Collin Van Dyck <collinvandyck@gmail.com>
2023-04-15 12:34:36 -0400

    Fix LSP format on save.

--- nvim/lua/init_autocmds.lua
+++ nvim/lua/init_autocmds.lua
@@ -1,7 +1,7 @@
 vim.cmd([[
 augroup GoGroup
   autocmd!
-  autocmd BufWritePre *.go :silent! lua vim.lsp.buf.formatting_sync(nil, 10000)
+  autocmd BufWritePre *.go :silent! lua vim.lsp.buf.format(nil, 10000)
   autocmd BufWritePre *.go lua vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
 augroup END
 ]])
