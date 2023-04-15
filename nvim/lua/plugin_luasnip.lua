9436eb904d5ace8c1799e7549af9dda938d43814
Collin Van Dyck <collinvandyck@gmail.com>
2023-04-15 16:35:19 -0400

    Start using my own snippets.

--- nvim/lua/plugin_luasnip.lua
+++ nvim/lua/plugin_luasnip.lua
@@ -1,4 +1,4 @@
-require("luasnip/loaders/from_vscode").load({ include = { "go" } })
+require("luasnip/loaders/from_lua").load({ paths = {"~/.config/nvim/snippets"} })
 
 -- press <Tab> to expand or jump in a snippet.
 vim.api.nvim_set_keymap('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : luasnip#expand_or_jumpable() ? "\\<Plug>luasnip-expand-or-jump" : "\\<Tab>"', {expr = true, silent = true})
