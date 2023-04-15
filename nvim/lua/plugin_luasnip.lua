013ac5a90c4200dcd5090eaa7b582e8ec6a1f53f
Collin Van Dyck <collinvandyck@gmail.com>
2023-04-15 17:07:16 -0400

    Better mappings for moving around in snippets

--- nvim/lua/plugin_luasnip.lua
+++ nvim/lua/plugin_luasnip.lua
@@ -1,13 +1,9 @@
 require("luasnip/loaders/from_lua").load({ paths = {"~/.config/nvim/snippets"} })
 
--- press <Tab> to expand or jump in a snippet.
-vim.api.nvim_set_keymap('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : luasnip#expand_or_jumpable() ? "\\<Plug>luasnip-expand-or-jump" : "\\<Tab>"', {expr = true, silent = true})
-vim.api.nvim_set_keymap('i', '<S-Tab>', '<cmd>lua require("luasnip").jump(-1)<Cr>', {silent = true})
-vim.api.nvim_set_keymap('s', '<Tab>', '<cmd>lua require("luasnip").jump(1)<Cr>', {silent = true})
-vim.api.nvim_set_keymap('s', '<S-Tab>', '<cmd>lua require("luasnip").jump(-1)<Cr>', {silent = true})
-
--- For changing choices in choiceNodes (not strictly necessary for a basic setup).
-vim.api.nvim_set_keymap('i', '<C-E>', 'luasnip#choice_active() ? "\\<Plug>luasnip-next-choice" : "\\<C-E>"', {expr = true, silent = true})
-vim.api.nvim_set_keymap('s', '<C-E>', 'luasnip#choice_active() ? "\\<Plug>luasnip-next-choice" : "\\<C-E>"', {expr = true, silent = true})
-
+local keymap = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+keymap("i", "<m-j>", "<cmd>lua require('luasnip').jump(1)<CR>", { silent = true, noremap = true})
+keymap("i", "<m-k>", "<cmd>lua require('luasnip').jump(-1)<CR>", { silent = true, noremap = true})
+keymap("s", "<m-j>", "<cmd>lua require('luasnip').jump(1)<CR>", { silent = true, noremap = true})
+keymap("s", "<m-k>", "<cmd>lua require('luasnip').jump(-1)<CR>", { silent = true, noremap = true})
 
