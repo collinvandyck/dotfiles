c5e056bc181e339ff8e2b5ac0561c22f12310081
Collin Van Dyck <collinvandyck@gmail.com>
2023-04-17 13:30:15 +0000

    Add custom fzf theme

--- omz/custom/fzf.zsh
+++ omz/custom/fzf.zsh
@@ -17,9 +17,10 @@ export FZF_DEFAULT_OPTS="\
 
 # dracula theme.
 # https://draculatheme.com/fzf
-
 FZF_DRACULA="--color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9 --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9 --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6 --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4"
-export FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} ${FZF_DRACULA}"
+FZF_CUSTOM="--color=fg:#d0d0d0,bg:#121212,hl:#5f87af --color=fg+:#d0d0d0,bg+:#262626,hl+:#5fd7ff --color=info:#afaf87,prompt:#d7005f,pointer:#af5fff --color=marker:#87ff00,spinner:#af5fff,header:#87afaf"
+FZF_THEME=$FZF_DRACULA
+export FZF_DEFAULT_OPTS="${FZF_DEFAULT_OPTS} ${FZF_THEME}"
 
 # other options
 #
