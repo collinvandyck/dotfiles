b4e707a028f123ba3ba3fba1e8295edd76df9ba0
Collin Van Dyck <collinvandyck@gmail.com>
2023-05-18 13:53:14 +0000

    Use native vim api to define autocmds

--- nvim/lua/init_autocmds.lua
+++ nvim/lua/init_autocmds.lua
@@ -1,27 +1,27 @@
 -- startup commands
+vim.api.nvim_create_augroup("Startup", {clear=true})
+vim.api.nvim_create_autocmd({"VimEnter"}, {
+	group = "Startup",
+	pattern = {"*"},
+	callback = function(ev)
+	  local bufnr = vim.api.nvim_get_current_buf()
+	  local buf_name = vim.api.nvim_buf_get_name(bufnr)
+	  local buf_line_count = vim.api.nvim_buf_line_count(bufnr)
 
-vim.cmd([[
-  augroup StartupCommands
-    autocmd!
-    autocmd VimEnter * lua check_empty_and_toggle_tree()
-  augroup END
-]])
+	  if buf_name == "" and buf_line_count == 1 then
+		vim.cmd("silent! NvimTreeToggle")
+		vim.cmd("wincmd l")
+	  end
+	end,
+})
 
-function check_empty_and_toggle_tree()
-  local bufnr = vim.api.nvim_get_current_buf()
-  local buf_name = vim.api.nvim_buf_get_name(bufnr)
-  local buf_line_count = vim.api.nvim_buf_line_count(bufnr)
+vim.api.nvim_create_augroup("GoGroup", {clear=true})
+vim.api.nvim_create_autocmd({"BufWritePre"}, {
+	group = "GoGroup",
+	pattern = {"*.go"},
+	callback = function(ev)
+		vim.lsp.buf.format(nil, 10000)
+		vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
+	end,
+})
 
-  if buf_name == "" and buf_line_count == 1 then
-    vim.cmd("silent! NvimTreeToggle")
-    vim.cmd("wincmd l")
-  end
-end
-
-vim.cmd([[
-augroup GoGroup
-  autocmd!
-  autocmd BufWritePre *.go :silent! lua vim.lsp.buf.format(nil, 10000)
-  autocmd BufWritePre *.go lua vim.lsp.buf.code_action({ context = { only = { 'source.organizeImports' } }, apply = true })
-augroup END
-]])
