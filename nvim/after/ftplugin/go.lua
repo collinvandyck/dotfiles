d682e57e47c0eda223a47441b19892c1c12ef53c
Collin Van Dyck <collinvandyck@gmail.com>
2023-05-18 13:03:02 +0000

    Move startup commands into one place.

--- nvim/after/ftplugin/go.lua
+++ nvim/after/ftplugin/go.lua
@@ -1,6 +1,7 @@
 vim.bo.expandtab = false
 vim.bo.tabstop = 4
 vim.bo.omnifunc = "v:lua.vim.lsp.omnifunc"
+vim.bo.smartindent = true
 
 vim.api.nvim_buf_set_keymap(0, 'n', vim.g.maplocalleader .. 'c', 'I//<esc>', {noremap = true, silent = true})
 
