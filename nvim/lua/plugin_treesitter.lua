fa26c2e0c0fef269b03a5d733426ab1888250dc2
Collin Van Dyck <collinvandyck@gmail.com>
2023-06-11 11:37:01 -0400

    Update install scripts for linux/ubuntu:latest.

--- nvim/lua/plugin_treesitter.lua
+++ nvim/lua/plugin_treesitter.lua
@@ -6,7 +6,7 @@ require'nvim-treesitter.configs'.setup {
   ensure_installed = "all",
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
+  sync_install = true,
 
   -- List of parsers to ignore installing (for "all")
   ignore_install = { },
