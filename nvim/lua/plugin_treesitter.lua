329537981d5c306509b9ce30d4363f9ec9b858d3
Collin Van Dyck <collinvandyck@gmail.com>
2023-06-15 19:05:12 -0400

    Install treesitter async.

--- nvim/lua/plugin_treesitter.lua
+++ nvim/lua/plugin_treesitter.lua
@@ -6,7 +6,7 @@ require'nvim-treesitter.configs'.setup {
   ensure_installed = "all",
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = true,
+  sync_install = false,
 
   -- List of parsers to ignore installing (for "all")
   ignore_install = { },
