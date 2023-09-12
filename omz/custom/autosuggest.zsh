667626c6711025c862f1ad773de7253e4d7d5870
Collin Van Dyck <collinvandyck@gmail.com>
2023-09-11 21:18:30 -0400

    Some adjustments.

---
+++ omz/custom/autosuggest.zsh
@@ -0,0 +1,7 @@
+# for zsh-autosuggestions
+#
+# See: https://github.com/zsh-users/zsh-autosuggestions
+
+# ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#ff00ff,bg=cyan,bold,underline"
+ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+
