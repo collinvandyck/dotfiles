0ad8e73ac1de8c0aa6610e6f68982f7e4b142dc8
Collin Van Dyck <collinvandyck@gmail.com>
2023-10-25 10:03:21 -0400

    Copy yanked text to neovim clipboard.

--- nvim/lua/plugin_osc52.lua
+++ nvim/lua/plugin_osc52.lua
@@ -5,3 +5,18 @@ require('osc52').setup {
   silent     = false,  -- Disable message on successful copy
   trim       = false,  -- Trim surrounding whitespaces before copy
 }
+
+-- not used, copied from examples.
+function copy_plus()
+  if vim.v.event.operator == 'y' and vim.v.event.regname == '+' then
+    require('osc52').copy_register('+')
+  end
+end
+
+function copy()
+  if vim.v.event.operator == 'y' then
+    require('osc52').copy_register(vim.v.event.regname)
+  end
+end
+
+vim.api.nvim_create_autocmd('TextYankPost', {callback = copy})
