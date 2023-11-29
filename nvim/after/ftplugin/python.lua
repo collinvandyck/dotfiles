181b976572aedef17e3b72b3872c9a0d888e8b35
Collin Van Dyck <collinvandyck@gmail.com>
2023-11-28 22:23:23 -0500

    Add python ftplugin.

--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -1,2 +1,8 @@
 vim.api.nvim_buf_set_keymap(0, 'n', vim.g.maplocalleader .. 'c', 'I#<esc>', {noremap = true, silent = true})
 
+vim.bo.autoindent = true
+vim.bo.expandtab = true
+vim.bo.shiftwidth = 2
+vim.bo.smartindent = true
+vim.bo.tabstop = 2
+
