f1ffc9e51727d560859d90aeb2d827924ff2a1d7
Collin Van Dyck <collinvandyck@gmail.com>
2024-01-03 16:13:37 -0500

    Add gl function
    Unfortunately this conflicts with the gl built-in alias. Still trying to figure out how to unset that on older powershells.

--- windows/profile.ps1
+++ windows/profile.ps1
@@ -3,6 +3,7 @@ Set-PSReadLineOption -EditMode Emacs
 function gf { git fetch }
 function gb { git branch }
 function gst { git status }
+function gl { git pull --rebase }
 function gll { git log --pretty=oneline --abbrev-commit --decorate --graph }
 function glla { git log --pretty=oneline --abbrev-commit --decorate --graph --all }
 function gllc { gll --glob="refs/*/collin/*" --glob="refs/*/main" --glob="refs/*/master" }
