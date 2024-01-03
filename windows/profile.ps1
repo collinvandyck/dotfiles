418b7d33660388f46c65ebd31b0a98f326496b65
Collin Van Dyck <collinvandyck@gmail.com>
2024-01-03 13:33:29 -0500

    Start a standard powershell profile.

---
+++ windows/profile.ps1
@@ -0,0 +1,14 @@
+Set-PSReadLineOption -EditMode Emacs
+
+function gf { git fetch }
+function gb { git branch }
+function gst { git status }
+function gll { git log --pretty=oneline --abbrev-commit --decorate --graph }
+function glla { git log --pretty=oneline --abbrev-commit --decorate --graph --all }
+function gllc { gll --glob="refs/*/collin/*" --glob="refs/*/main" --glob="refs/*/master" }
+function gcav { git add -A; git commit -v }
+
+set-alias bat           get-content
+set-alias vi            nvim
+set-alias vim           nvim
+
