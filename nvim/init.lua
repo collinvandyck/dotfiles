1cf91528ce159eace2ee249c680ecd5f6a858a7b
Collin Van Dyck <collinvandyck@gmail.com>
2024-03-08 14:55:31 -0500

    Change comment highlighting.

--- nvim/init.lua
+++ nvim/init.lua
@@ -841,6 +841,7 @@ require("lazy").setup({
 
 -- Note: use :Inspect to find the highlight groups for a particular code block.
 vim.cmd('colorscheme tokyonight-night')
+vim.cmd [[highlight Comment guisp=#8a8a8a guifg=#8a8a8a]]
 vim.cmd [[highlight DiagnosticUnderlineHint gui=undercurl guisp=#8a8a8a guifg=#8a8a8a]]
 vim.cmd [[highlight DiagnosticUnnecessary gui=undercurl guisp=#8a8a8a guifg=#8a8a8a]]
 vim.cmd [[highlight TabLine guisp=#8a8a8a guifg=#8a8a8a]]
