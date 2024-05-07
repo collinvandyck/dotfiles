e1b0076d20c3eaa8ed71d886f06949a8a9cef956
Collin Van Dyck <collinvandyck@gmail.com>
2024-05-07 10:16:11 -0400

    tmux improvements
    - tmuxinator config
    - work specific scripts/aliases
    
    still a bit of work to do to get my tmux workflow right, but tmuxinator is going
    to really help.

---
+++ tmux/hooks/detach.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# detach.sh
+
+set -e # exit on failure
+set -u # fail if vars are not defined
+
---
+++ tmux/hooks/session-closed.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# session-closed.sh
+
+set -e # exit on failure
+set -u # fail if vars are not defined
+
