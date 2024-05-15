8791d083fc7c8c66d9a4aca9e7b3d684d5c5912c
Collin Van Dyck <collinvandyck@gmail.com>
2024-05-15 09:45:29 -0400

    Ensure nvim-tree is deleted before saving sessions.
    This makes it nice when restoring.

--- nvim/init.lua
+++ nvim/init.lua
@@ -909,12 +909,27 @@ require("lazy").setup({
 					theme_conf = { border = true },
 					previewer = false,
 				},
+				pre_save_cmds = {
+					function()
+						-- delete all nvim-tree buffers before saving the
+						-- session
+						local function close_all_nvim_trees()
+							local buf_delete = vim.api.nvim_buf_delete
+							local buf_list = vim.api.nvim_list_bufs()
+
+							for _, buf in ipairs(buf_list) do
+								local buftype = vim.api.nvim_buf_get_option(buf, "ft")
+								if buftype == "NvimTree" then
+									pcall(buf_delete, buf, {})
+								end
+							end
+						end
+						close_all_nvim_trees()
+					end,
+				},
 				post_restore_cmds = { function()
-					local nt_api = require("nvim-tree.api")
-					if require('nvim-tree.view').is_visible() then
-						nt_api.tree.open({ focus = false })
-						vim.notify("opened!")
-					end
+					--local nt_api = require("nvim-tree.api")
+					--nt_api.tree.reload()
 				end }
 			}
 		end,
