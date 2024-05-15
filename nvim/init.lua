146c298be1585d255155ca62e006ecf0004e81bf
Collin Van Dyck <collinvandyck@gmail.com>
2024-05-15 13:04:22 -0400

    Add nvim-surround plugin.

--- nvim/init.lua
+++ nvim/init.lua
@@ -934,6 +934,16 @@ require("lazy").setup({
 			}
 		end,
 	},
+	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({
+				-- Configuration here, or leave empty to use defaults
+			})
+		end
+	},
 	"nanotee/zoxide.vim",
 	"NoahTheDuke/vim-just",
 	"timmyjose-projects/lox.vim",
