cfd9bc244cd9d927220c3d3aaa132795e10c0cda
Collin Van Dyck <collinvandyck@gmail.com>
2024-05-20 10:09:49 -0400

    Adjust dressing.nvim, cleanup cruft.

--- nvim/init.lua
+++ nvim/init.lua
@@ -949,6 +949,7 @@ require("lazy").setup({
 		config = function()
 			require('dressing').setup({
 				input = {
+					relative = "win",
 					win_options = {
 						-- winhighlight = 'NormalFloat:DiagnosticError'
 					}
@@ -956,30 +957,9 @@ require("lazy").setup({
 			})
 		end
 	},
-	{
-		"kevinhwang91/nvim-ufo",
-		dependencies = {
-			"kevinhwang91/promise-async",
-		},
-		enabled = false,
-		config = function()
-			--vim.o.foldcolumn = '1' -- '0' is not bad
-			--vim.o.foldlevel = 99 -- Using ufo provider need a large value, feel free to decrease the value
-			--vim.o.foldlevelstart = 99
-			--vim.o.foldenable = true
-			--vim.keymap.set('n', 'zR', require('ufo').openAllFolds)
-			--vim.keymap.set('n', 'zM', require('ufo').closeAllFolds)
-			--require('ufo').setup({
-			--provider_selector = function(bufnr, filetype, buftype)
-			--return { 'treesitter', 'indent' }
-			--end
-			--})
-		end
-	},
 	"nanotee/zoxide.vim",
 	"NoahTheDuke/vim-just",
 	"timmyjose-projects/lox.vim",
-	--"chrisbra/Colorizer",
 	"mtdl9/vim-log-highlighting",
 })
 
