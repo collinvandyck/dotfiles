08e166359211f93ed4de3d236130ff25bc74dbec
Collin Van Dyck <collinvandyck@gmail.com>
2024-05-24 10:29:21 -0400

    Update plugins, disable lsp lines/saga for now.

--- nvim/lua/plugins/lsp-lines.lua
+++ nvim/lua/plugins/lsp-lines.lua
@@ -1,11 +1,12 @@
 -- https://git.sr.ht/~whynothugo/lsp_lines.nvim
-vim.diagnostic.config({
-	virtual_lines = { only_current_line = true },
-	virtual_text = true,
-})
 return {
+	enabled = false,
 	"https://git.sr.ht/~whynothugo/lsp_lines.nvim",
 	config = function()
+		vim.diagnostic.config({
+			virtual_lines = { only_current_line = true },
+			virtual_text = true,
+		})
 		require("lsp_lines").setup()
 	end,
 }
