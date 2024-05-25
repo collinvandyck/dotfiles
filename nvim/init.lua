40af7676e0d8072d08792e680b0d569822ec76df
Collin Van Dyck <collinvandyck@gmail.com>
2024-05-25 11:26:45 -0400

    Move lazy framework initialization into own module.

--- nvim/init.lua
+++ nvim/init.lua
@@ -51,20 +51,7 @@ vim.opt.wrap = false
 vim.g.mapleader = " "
 vim.g.maplocalleader = "-"
 
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable", -- latest stable release
-		lazypath,
-	})
-end
-vim.opt.rtp:prepend(lazypath)
-
-require("lazy").setup("plugins")
+require("start_lazy")
 
 -- important to set this after lazy has finished loading
 -- vim.cmd('colorscheme moonfly')
