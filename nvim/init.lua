adbc85b752bf703ea874131c6acf2a20a2b75f6c
Collin Van Dyck <collinvandyck@gmail.com>
2024-05-25 11:50:18 -0400

    Move colorscheme setup into a colors module.

--- nvim/init.lua
+++ nvim/init.lua
@@ -46,14 +46,7 @@ require("plugs")
 require("commands")
 require("abbreviations")
 require("autocmds")
-
--- Note: use :Inspect to find the highlight groups for a particular code block.
-vim.cmd('colorscheme tokyonight-night')
-vim.cmd [[highlight Comment guisp=#8a8a8a guifg=#8a8a8a]]
-vim.cmd [[highlight DiagnosticUnderlineHint gui=undercurl guisp=#8a8a8a guifg=#8a8a8a]]
-vim.cmd [[highlight DiagnosticUnnecessary gui=undercurl guisp=#8a8a8a guifg=#8a8a8a]]
-vim.cmd [[highlight TabLine guisp=#8a8a8a guifg=#8a8a8a]]
-vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = "#6CC644" })
+require("colors")
 
 -- q leader key maps
 function ToggleQuickfix()
