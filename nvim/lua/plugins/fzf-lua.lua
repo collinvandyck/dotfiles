5852cacd4a11a84d39f8554ff8f866d5c20d576f
Collin Van Dyck <collinvandyck@gmail.com>
2024-06-25 09:53:42 -0400

    Update fzf-lua with a more generous lsp timeout.

--- nvim/lua/plugins/fzf-lua.lua
+++ nvim/lua/plugins/fzf-lua.lua
@@ -1,15 +1,14 @@
 return {
     "ibhagwan/fzf-lua",
-    -- optional for icon support
-    dependencies = { "nvim-tree/nvim-web-devicons" },
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
     config = function()
-        -- calling `setup` is optional for customization
         require("fzf-lua").setup({
-            fzf_opts = {
-                --['--history'] =  vim.fn.stdpath("data") .. '/fzf-lua-history',
+            file_icon_padding = ' ', -- helps with kitty rendering
+            lsp = {
+                async_or_timeout = 10000,
             },
-            -- required to render icons correctly with kitty
-            file_icon_padding = ' ',
         })
     end
 }
