a6611fdc80bff36a17b30785f6ab40b8cbdae8ff
Collin Van Dyck <collinvandyck@gmail.com>
2024-06-26 19:50:43 -0400

    Add autocmd to resize after fugitive buffer is unloaded.

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,41 +1,53 @@
+local group = vim.api.nvim_create_augroup("StandardAutoCommands", { clear = true })
+
 -- if while closing a buffer we only have one normal buffer left, close
 -- nvim-tree so that the tab is destroyed.
-local quit_group = vim.api.nvim_create_augroup("QuitHooks", { clear = true })
 vim.api.nvim_create_autocmd({ "QuitPre" }, {
-    group = quit_group,
-    pattern = { "*" },
-    callback = function()
-        if vim.bo.filetype == 'fugitiveblame' then
-            -- don't do anything if we're closing a blame buffer
-            return
-        end
-        local tab = vim.api.nvim_get_current_tabpage()
-        local wins = vim.api.nvim_tabpage_list_wins(tab)
-        local normals = 0
-        local nvim_tree_win = nil
-        for _, win in ipairs(wins) do
-            local buf = vim.api.nvim_win_get_buf(win);
-            local ft = vim.api.nvim_buf_get_option(buf, 'filetype');
-            local bt = vim.api.nvim_buf_get_option(buf, 'buftype');
-            if ft == 'NvimTree' then
-                nvim_tree_win = win
-            elseif bt == "" then
-                normals = normals + 1
-            end
-        end
-        if nvim_tree_win and normals == 1 then
-            vim.api.nvim_win_close(nvim_tree_win, false)
-        end
-    end,
+	group = group,
+	pattern = { "*" },
+	callback = function()
+		if vim.bo.filetype == 'fugitiveblame' then
+			-- don't do anything if we're closing a blame buffer
+			return
+		end
+		local tab = vim.api.nvim_get_current_tabpage()
+		local wins = vim.api.nvim_tabpage_list_wins(tab)
+		local normals = 0
+		local nvim_tree_win = nil
+		for _, win in ipairs(wins) do
+			local buf = vim.api.nvim_win_get_buf(win);
+			local ft = vim.api.nvim_buf_get_option(buf, 'filetype');
+			local bt = vim.api.nvim_buf_get_option(buf, 'buftype');
+			if ft == 'NvimTree' then
+				nvim_tree_win = win
+			elseif bt == "" then
+				normals = normals + 1
+			end
+		end
+		if nvim_tree_win and normals == 1 then
+			vim.api.nvim_win_close(nvim_tree_win, false)
+		end
+	end,
 })
 
 -- highlight on yank
-local highlight_group = vim.api.nvim_create_augroup("HighlightGroup", { clear = true })
-vim.api.nvim_create_autocmd("TextYankPost",
-    {
-        group = highlight_group,
-        pattern = "*",
-        callback = function()
-            vim.highlight.on_yank()
-        end,
-    })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	group = group,
+	pattern = "*",
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+})
+
+-- after a fugitiveblame buffer is unloaded, resize. the delay works around the
+-- lack of a precise event to do this.
+vim.api.nvim_create_autocmd("BufUnload", {
+	pattern = "*",
+	callback = function()
+		if vim.bo.filetype == "fugitiveblame" then
+			vim.defer_fn(function()
+				vim.cmd('wincmd =')
+			end, 10)
+		end
+	end,
+})
