f4f2c520884143b9a7996bf5e5313d5c09aeedff
Collin Van Dyck <collinvandyck@gmail.com>
2024-06-30 11:02:47 -0400

    Move widgets into widgets.zsh

---
+++ zsh/widgets.zsh
@@ -0,0 +1,25 @@
+# fix for zsh alt left / right not working while in tmux
+bindkey "^[[1;3C" forward-word
+bindkey "^[[1;3D" backward-word
+
+bindkey '\e' vi-cmd-mode 
+#KEYTIMEOUT=1 # 100ms
+
+prepend-sudo() {
+  if [[ $BUFFER != "sudo "* ]]; then
+    BUFFER="sudo $BUFFER"; CURSOR+=5
+  fi
+}
+
+zle -N prepend-sudo
+bindkey -M vicmd s prepend-sudo
+bindkey "^[su" prepend-sudo
+
+# expand aliases on current command line
+globalias() {
+	zle _expand_alias
+	zle expand-word
+	#zle self-insert
+}
+zle -N globalias
+bindkey -M emacs "^ " globalias
