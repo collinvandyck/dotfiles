3fb891961e1b2f1c643c57077ca013b6f0aacca1
Collin Van Dyck <collinvandyck@gmail.com>
2024-07-04 17:01:25 -0400

    Configure lualine to take over the tab bar.

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -5,13 +5,55 @@ return {
 		"nvim-tree/nvim-web-devicons",
 		"folke/tokyonight.nvim"
 	},
-	opts = {
-		options = {
-			theme = 'tokyonight',
-		},
-		sections = {
-			lualine_b = { 'diff', 'diagnostics' },
-			lualine_c = { { 'filename', path = 3, } },
-		},
-	},
+	config = function()
+		require('lualine').setup({
+			options = {
+				icons_enabled = true,
+				theme = 'tokyonight',
+				component_separators = { left = '', right = '' },
+				section_separators = { left = '', right = '' },
+				disabled_filetypes = {
+					statusline = {},
+					winbar = {},
+				},
+				ignore_focus = {},
+				always_divide_middle = true,
+				globalstatus = false,
+				refresh = {
+					statusline = 1000,
+					tabline = 1000,
+					winbar = 1000,
+				}
+			},
+			sections = {
+				lualine_a = {
+					{ 'mode', fmt = function(str) return str:sub(1, 2) end },
+				},
+				lualine_b = { 'diagnostics' },
+				lualine_c = { {
+					'filename', path = 3,
+				} },
+				lualine_x = { 'encoding', 'fileformat', 'filetype' },
+				lualine_y = { 'progress' },
+				lualine_z = { 'location' }
+			},
+			tabline = {
+				lualine_a = {
+					{
+						'tabs',
+						max_length = vim.o.columns,
+						mode = 2,
+						path = 1,
+					},
+				},
+				lualine_b = {},
+				lualine_c = {},
+				lualine_x = {},
+				lualine_y = {},
+				lualine_z = {}
+			},
+			inactive_winbar = {},
+			extensions = {},
+		})
+	end,
 }
