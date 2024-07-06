9da2d72f8e8f424c8739cff9f70d57a062aba0e0
Collin Van Dyck <collinvandyck@gmail.com>
2024-07-06 13:06:16 -0400

    Remove all fzf integrations except for fzf-lua.

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -1,4 +1,17 @@
 return {
-	"junegunn/fzf",
-	build = function() vim.call("fzf#install") end
+	"ibhagwan/fzf-lua",
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		require("fzf-lua").setup({
+			file_icon_padding = ' ', -- helps with kitty rendering
+			lsp = {
+				async_or_timeout = 10000,
+			},
+			winopts = {
+				width = 0.95,
+			}
+		})
+	end
 }
