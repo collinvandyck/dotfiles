1b35c4b3b747192a3df48321382c9c02551c4dc2
Collin Van Dyck <collinvandyck@gmail.com>
2024-07-06 19:27:50 -0400

    Add barbecue pluging to get vscode style breadcrumbs
    This lets me get rid of treesitter context.
    Unfortunately it's not compatible with icon overrides.
    But this seems like a fair trade.

--- nvim/lua/plugins/nvim-web-devicons.lua
+++ nvim/lua/plugins/nvim-web-devicons.lua
@@ -1,16 +1,5 @@
 return {
 	"nvim-tree/nvim-web-devicons",
 	opts = {
-		override_by_extension = {
-			["rs"] = {
-				icon = "🦀",
-			},
-			[".envrc"] = {
-				icon = "",
-				color = "#faf743",
-				cterm_color = "227",
-				name = "Envrc",
-			},
-		},
 	}
 }
