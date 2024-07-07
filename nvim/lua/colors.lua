d7bbd0dd9cd72e64d94311c2971319a2abd55350
Collin Van Dyck <collinvandyck@gmail.com>
2024-07-07 13:53:43 -0400

    Move all color init into tokyonight plugin hooks.
    This is great. Much better. Once place to manage overrides.

--- nvim/lua/colors.lua
+++
@@ -1,9 +0,0 @@
--- Note: use :Inspect to find the highlight groups for a particular code block.
-vim.cmd([[
-	colorscheme tokyonight-night
-	highlight Comment guisp=#8a8a8a guifg=#8a8a8a
-	highlight DiagnosticUnderlineHint gui=undercurl guisp=#8a8a8a guifg=#8a8a8a
-	highlight DiagnosticUnnecessary gui=undercurl guisp=#8a8a8a guifg=#8a8a8a
-	highlight TabLine guisp=#8a8a8a guifg=#8a8a8a
-]])
-vim.api.nvim_set_hl(0, "CmpItemKindCopilot", { fg = "#6CC644" })
--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -7,6 +7,7 @@ return {
 		require("tokyonight").setup({
 			style = "night",
 			on_colors = function(colors)
+				-- Note: use :Inspect to find the highlight groups for a particular code block.
 				-- https://github.com/folke/tokyonight.nvim/blob/main/extras/lua/tokyonight_night.lua
 				-- https://mdigi.tools/lighten-color
 				-- https://mdigi.tools/darken-color
@@ -17,11 +18,21 @@ return {
 				colors.bg_visual = "#283457"
 				colors.bg_visual = "#40538b"
 			end,
-			on_highlights = function(highlights)
+			on_highlights = function(hl)
 				-- darken the bg for the cursorline
-				highlights.CursorLine.bg = "#292e42"
-				highlights.CursorLine.bg = "#1f2231" -- 25% darker
-				highlights.CursorLine.bg = "#1d202e" -- 30% darker
+				hl.CursorLine.bg = "#292e42"
+				hl.CursorLine.bg = "#1f2231" -- 25% darker
+				hl.CursorLine.bg = "#1d202e" -- 30% darker
+
+				hl.Comment.fg = "#8a8a8a"
+				hl.DiagnosticUnderlineHint.sp = "#8a8a8a"
+				hl.DiagnosticUnnecessary.fg = "#8a8a8a"
+				--hl.TabLine.fg = "#8a8a8a"
+
+				-- this can be nil i think if copilot is not configured in nvim-cmp
+				if hl.CmpItemKindCopilot then
+					hl.CmpItemKindCopilot.fg = "#6CC644"
+				end
 			end,
 			lualine_bold = true,
 		})
