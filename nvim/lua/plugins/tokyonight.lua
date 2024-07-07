a0b96130098aa2128a2a3631fd0d50247de9e6bc
Collin Van Dyck <collinvandyck@gmail.com>
2024-07-07 13:13:30 -0400

    Adjust tokyonight backgrounds.
    - visual mode background is brightened
    - cursorline background is darkened

--- nvim/lua/plugins/tokyonight.lua
+++ nvim/lua/plugins/tokyonight.lua
@@ -7,7 +7,21 @@ return {
 		require("tokyonight").setup({
 			style = "night",
 			on_colors = function(colors)
+				-- https://github.com/folke/tokyonight.nvim/blob/main/extras/lua/tokyonight_night.lua
+				-- https://mdigi.tools/lighten-color
+				-- https://mdigi.tools/darken-color
+
 				colors.bg = "#0e0e13"
+
+				-- brighten the bg in visual mode
+				colors.bg_visual = "#283457"
+				colors.bg_visual = "#40538b"
+			end,
+			on_highlights = function(highlights)
+				-- darken the bg for the cursorline
+				highlights.CursorLine.bg = "#292e42"
+				highlights.CursorLine.bg = "#1f2231" -- 25% darker
+				highlights.CursorLine.bg = "#1d202e" -- 30% darker
 			end,
 			lualine_bold = true,
 		})
