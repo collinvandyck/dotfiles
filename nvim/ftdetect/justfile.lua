b34360be89c56f09b0e611b0fcc6ff15f36f7f7b
Collin Van Dyck <collinvandyck@gmail.com>
2024-09-01 22:43:16 -0400

    Better setup for justfiles.

--- nvim/ftdetect/justfile.lua
+++ nvim/ftdetect/justfile.lua
@@ -1,5 +1,15 @@
 vim.filetype.add({
 	filename = {
-		['justfile'] = 'make',
+		['justfile'] = 'just',
 	},
+})
+
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "just",
+	callback = function()
+		vim.opt_local.textwidth = 80
+		-- Inherit 'make' settings:
+		vim.cmd('runtime! ftplugin/make.vim')
+		vim.cmd('runtime! syntax/make.vim')
+	end
 })
