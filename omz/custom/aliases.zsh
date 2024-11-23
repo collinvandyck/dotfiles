a085bb5007d4723223af6842a8acca5ef74c3dfa
Collin Van Dyck <collinvandyck@gmail.com>
2024-11-23 17:01:45 -0500

    alias a=z for no good reason.

--- omz/custom/aliases.zsh
+++ omz/custom/aliases.zsh
@@ -6,6 +6,7 @@ alias sshctl='ll ~/.ssh/control/'
 alias xx='ssh -t 5xx.engineer tm'
 alias ut='ssh -t ubuntu-test tm'
 alias appsup='cd ~/Library/Application\ Support'
+alias a=z
 
 [ "$TERM" = "xterm-kitty" ] && alias ssh="kitty +kitten ssh"
 
