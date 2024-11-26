1845bd33a5795ca61fd9495d6b5d14bbb0b27fde
Collin Van Dyck <collinvandyck@gmail.com>
2024-11-26 10:51:46 -0500

    Set up binding for zsh-autosuggestions.

--- omz/custom/autosuggest.zsh
+++ omz/custom/autosuggest.zsh
@@ -2,6 +2,7 @@
 #
 # See: https://github.com/zsh-users/zsh-autosuggestions
 
-# ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#ff00ff,bg=cyan,bold,underline"
-ZSH_AUTOSUGGEST_STRATEGY=(history completion)
+#ZSH_AUTOSUGGEST_HIGHLIGHT_STYLE="fg=#ff00ff,bg=cyan,bold,underline"
+#ZSH_AUTOSUGGEST_STRATEGY=(history completion)
 
+bindkey '^o' autosuggest-accept
