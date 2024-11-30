3daba2cbdcfe86f4f26de6b8ce9543e5b588e8f7
Collin Van Dyck <collinvandyck@gmail.com>
2024-11-29 22:19:50 -0500

    Adjust python buffer settings.

--- nvim/after/ftplugin/python.lua
+++ nvim/after/ftplugin/python.lua
@@ -1,6 +1,5 @@
 vim.api.nvim_buf_set_keymap(0, 'n', vim.g.maplocalleader .. 'c', 'I#<esc>', { noremap = true, silent = true })
-
 vim.bo.autoindent = true
-vim.bo.shiftwidth = 2
+vim.bo.shiftwidth = 4
 vim.bo.smartindent = true
-vim.bo.tabstop = 2
+vim.bo.tabstop = 4
