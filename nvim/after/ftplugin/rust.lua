b22f125f65a76f1a61fa034cba8aad45a01e6c8b
Collin Van Dyck <collinvandyck@gmail.com>
2024-12-30 12:20:10 -0500

    Better pairs for rust.

--- nvim/after/ftplugin/rust.lua
+++ nvim/after/ftplugin/rust.lua
@@ -1,2 +1,23 @@
 vim.bo.textwidth = 99
-vim.keymap.set('i', "'", "'", { buffer = 0 })
+--vim.keymap.set('i', "'", "'", { buffer = 0 })
+
+local MiniPairs = require('mini.pairs')
+
+-- handle '' but disable for lifetimes
+MiniPairs.map_buf(0, 'i', "'", {
+	action = 'closeopen',
+	pair = "''",
+	neigh_pattern = '[^%a\\&<].',
+})
+
+-- handle <Generic> types
+MiniPairs.map_buf(0, 'i', '<', {
+	action = 'open',
+	pair = '<>',
+	neigh_pattern = '[^ ].',
+})
+MiniPairs.map_buf(0, 'i', '>', {
+	action = 'close',
+	pair = '<>',
+	neigh_pattern = '[^ ].',
+})
