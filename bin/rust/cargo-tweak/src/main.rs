4fab56d105ac6f4a688a99a22ee7c24bbf1cea34
Collin Van Dyck <collin@5xx.engineer>
2025-02-16 15:02:13 -0500

    Fix cargo-tweak to create lints.rust table implicitly.

--- bin/rust/cargo-tweak/src/main.rs
+++ bin/rust/cargo-tweak/src/main.rs
@@ -24,7 +24,18 @@ fn main() -> Result<()> {
     if let Some(allow) = &args.allow_unused {
         dirty = true;
         if *allow {
-            toml["lints"]["rust"]["unused"] = toml_edit::value("allow");
+            let lints = toml
+                .as_table_mut()
+                .entry("lints")
+                .or_insert_with(|| {
+                    let mut table = toml_edit::table();
+                    table.as_table_mut().unwrap().set_implicit(true);
+                    table
+                })
+                .as_table_mut()
+                .unwrap();
+            lints.insert("rust", toml_edit::table());
+            lints["rust"]["unused"] = "allow".into();
         } else {
             if let Some(rust) = toml.get_mut("lints").and_then(|f| f.get_mut("rust")) {
                 if let Some(rust) = rust.as_table_like_mut() {
@@ -48,4 +59,84 @@ fn write_toml(path: &Path, doc: &DocumentMut) -> Result<()> {
         .context("could not sync tempfile")?;
     fs::rename(&tmp, path).context("could not move tempfile to Cargo.toml")?;
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use toml_edit::{DocumentMut, Table};
+
+    macro_rules! formatted {
+        ($s:expr) => {{
+            $s.trim()
+                .lines()
+                .map(str::trim)
+                .collect::<Vec<_>>()
+                .join("\n")
+        }};
+    }
+
+    #[test]
+    fn parses_ok() {
+        let toml = formatted! {
+        r#"
+            "hello" = 'toml!'
+            [rust.lints]
+        "#
+        };
+        let doc: DocumentMut = toml.parse().unwrap();
+        assert_eq!(toml, doc.to_string().trim());
+    }
+
+    #[test]
+    fn simple_kv() {
+        let mut doc: DocumentMut = DocumentMut::new();
+        doc["foo"] = "bar".into();
+        assert_eq!(
+            doc.to_string().trim(),
+            formatted! {
+                r#"
+                    foo = "bar"
+                "#
+            }
+        );
+    }
+
+    #[test]
+    fn creates_table() {
+        let mut doc: DocumentMut = DocumentMut::new();
+        doc["foo"] = Table::new().into();
+        doc["foo"]["a"] = "b".into();
+        assert_eq!(
+            doc.to_string().trim(),
+            formatted! {
+                r#"
+                    [foo]
+                    a = "b"
+                "#
+            }
+        );
+    }
+
+    #[test]
+    fn creates_table_with_dotted_name() {
+        let mut doc: DocumentMut = DocumentMut::new();
+        let lints = doc
+            .as_table_mut()
+            .entry("lints")
+            .or_insert(toml_edit::table())
+            .as_table_mut()
+            .unwrap();
+        lints.set_implicit(true);
+        lints.insert("rust", toml_edit::table());
+        lints["rust"]["a"] = "b".into();
+        assert_eq!(
+            doc.to_string().trim(),
+            formatted! {
+                r#"
+                    [lints.rust]
+                    a = "b"
+                "#
+            }
+        );
+    }
 }
