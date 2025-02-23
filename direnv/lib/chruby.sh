65880b68d8af01e527a607a03c76f71b0f1b21d7
Collin Van Dyck <collin@5xx.engineer>
2025-02-23 18:49:26 -0500

    Add chruby / linux install.

--- direnv/lib/chruby.sh
+++ direnv/lib/chruby.sh
@@ -1,4 +1,12 @@
 use_chruby() {
-    source "$(brew --prefix)/opt/chruby/share/chruby/chruby.sh"
-    source "$(brew --prefix)/opt/chruby/share/chruby/auto.sh"
+    if command -v brew &>/dev/null; then
+        source "$(brew --prefix)/opt/chruby/share/chruby/chruby.sh"
+        source "$(brew --prefix)/opt/chruby/share/chruby/auto.sh"
+        return 0
+    fi
+    if [[ -f /usr/local/share/chruby/chruby.sh ]]; then
+        source /usr/local/share/chruby/chruby.sh
+        source /usr/local/share/chruby/auto.sh
+        return 0
+    fi
 }
