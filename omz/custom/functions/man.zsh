029daf863afacbbec80a17097377c5f7484d24d3
Collin Van Dyck <collin@5xx.engineer>
2025-03-11 09:21:46 -0400

    man function also handles programs that do not have man pages.

--- omz/custom/functions/man.zsh
+++ omz/custom/functions/man.zsh
@@ -1,8 +1,13 @@
 man() {
 	rename() { tmux rename-window "$@" &>/dev/null || true }
 	num-panes() { tmux display-message -p '#{window_panes}' || "0" }
+    man-help() { $1 --help | nvim '+Man!' }
 	local title=$(tmux display-message -p '#W' || true)
 	trap "rename '$title'" EXIT
 	[[ "$(num-panes)" == "1" ]] && rename "man $*"
-    command man "$@"
+    case $1 in
+        we) man-help watchexec ;;
+        watchexec|bat) man-help $1 ;;
+        *) command man "$@";;
+    esac
 }
