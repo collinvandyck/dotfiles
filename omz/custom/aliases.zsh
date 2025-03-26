75d4a53c22972d633f9d05783d56d2db85a36f7f
Collin Van Dyck <collin@5xx.engineer>
2025-03-26 17:24:26 -0400

    Add nman alias to view stdin as man page.

--- omz/custom/aliases.zsh
+++ omz/custom/aliases.zsh
@@ -11,6 +11,7 @@ alias cat=bat
 alias be='bundle exec'
 alias gw='gradlew'
 alias back='cd "$(zoxide query back)" && td '
+alias nman='nvim "+Man!"'
 
 [ "$TERM" = "xterm-kitty" ] && alias ssh="kitty +kitten ssh"
 
