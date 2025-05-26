cc1b95b21fd87446a628b5adf26184566584cac1
Collin Van Dyck <collin@5xx.engineer>
2025-05-26 12:29:09 -0400

    Set timeout len to 300.

--- nvim/init.lua
+++ nvim/init.lua
@@ -38,7 +38,7 @@ vim.opt.statusline = ""
 vim.opt.swapfile = false
 vim.opt.tabstop = 4
 vim.opt.termguicolors = true
-vim.opt.timeoutlen = 1000
+vim.opt.timeoutlen = 300
 vim.opt.textwidth = 0
 vim.opt.updatetime = 100
 vim.opt.wildignore:append('*.a')
