d626036fe0e5ad04036550bd89f27c2919720b16
Collin Van Dyck <collin@5xx.engineer>
2025-06-26 09:53:37 -0400

    Update firefox symlinking.

--- omz/custom/git.zsh
+++ omz/custom/git.zsh
@@ -1,19 +1,17 @@
 alias ci='git ci'
 alias cip='git cip'
 
-function delbranch()
-{
-    git branch -D $1 ; git push origin :refs/heads/$1
+function delbranch() {
+    git branch -D $1
+    git push origin :refs/heads/$1
 }
 
-function deltag()
-{
-    git tag -d $1 ; git push origin :refs/tags/$1
+function deltag() {
+    git tag -d $1
+    git push origin :refs/tags/$1
 }
 
-function gdom() {
-	git diff $(git merge-base HEAD origin/$(git_main_branch)) "$@"
-}
+function gdom() { git diff $(git merge-base HEAD origin/$(git_main_branch)) "$@" }
 
 function gcobr() {
     git checkout "$(git branch --all --sort=committerdate | fzf-tmux -p --no-sort --tac | tr -d '[:space:]' | tr -d '^\* ')"
