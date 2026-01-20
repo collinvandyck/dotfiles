ba0440b31e5cb7d898bf7fbf2367d770749227b4
Collin Van Dyck <collin@5xx.engineer>
2026-01-20 08:53:55 -0500

    updates to lima setup.

--- lima/default/setup.sh
+++ lima/default/setup.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 # Commands to be run on a fresh default lima instance
 
-if ! command -v mise &> /dev/null; then
-	curl https://mise.run | sh
-fi
-
+lima sh -c 'curl https://mise.run | sh'
+lima sudo snap install node --classic
+lima sudo npm install -g @anthropic-ai/claude-code
+#lima 'echo "eval \"$(/home/collin.linux/.local/bin/mise activate bash)\" >> /home/collin.linux/.bashrc"'
