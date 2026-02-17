6237744b4a9e33c63bb6d5a36a434bcbdde76482
Collin Van Dyck <collin@5xx.engineer>
2026-02-17 09:49:56 -0500

    Update worktrees funcs.

--- zsh/functions/worktrees.zsh
+++ zsh/functions/worktrees.zsh
@@ -1,82 +1,29 @@
-# an opinionated workflow on using git worktrees
-worktrees() {
-
-    usage="usage: $(basename "$0")"
-    error() { echo >&2 "error: $*" }
-    root() { echo "$(git rev-parse --git-common-dir)/.." }
-    branch() { echo "$USER/wt-$1" }
-    worktree-dir() { echo "$(root)/worktrees/$*" }
-
-    create-worktree() {
-        # TODO: link the .idea folder into the new worktree
-        # ln -s $(pwd)/.idea worktrees/cet-19921-xero-metrics/.idea
-
-        [[ $# -lt 1 ]] && error "$usage: create [name] [--force]" && return 1
-        name=$1; shift
-        br="-b"
-        commit=@
-        while [[ $# -gt 0 ]]; do
-            case $1 in
-                --force|-f) br="-B"
-                    ;;
-                *)
-                    [[ "$commit" != "@" ]] && error "cannot pass more than one ref" && return 1
-                    commit=$1
-                    ;;
-            esac
-            shift
-        done
-        branch="$(branch "$name")"
-        if [[ "$br" == "-b" ]] && git rev-parse $branch &>/dev/null; then
-            error "Branch $branch already exists. Use --force to override" && return 1
-        fi
-        root=$(root)
-        [[ ! -d "$root" ]] && error "not in a git repo!" && return 1
-        full_path="$root"/worktrees/"$name"
-        if [[ -d "$full_path" ]]; then
-            echo "Changing into existing worktree"
-        else
-            git worktree add "$full_path" "$br" "$branch" "$commit"
-        fi
-        cd "$full_path"
-    }
-
-switch-worktree() {
-    [[ $# -lt 1 ]] && error "$usage: switch [name] ..." && return 1
-    cd "$(worktree-dir "$1")"
-}
-
-remove-worktree() {
-    [[ $# -lt 1 ]] && error "$usage: remove [name] ..." && return 1
-    root=$(root)
-    git worktree remove "$@" || return $?
-    cd $root
-}
-
-list-worktrees() {
-    git worktree list
-}
-
-case "$1" in
-    "create")
-        shift
-        create-worktree "$@"
-        ;;
-    "remove")
-        shift
-        remove-worktree "$@"
-        ;;
-    "switch")
-        shift
-        switch-worktree "$@"
-        ;;
-    "list")
-        shift
-        list-worktrees "$@"
-        ;;
-
-    *)
-        list-worktrees
-        ;;
-esac
+# create a new worktree. assumes that you're in the repo
+new-worktree() {
+    if ! git rev-parse --git-common-dir &>/dev/null; then
+        echo "not in a git repo" >&2
+        return 1
+    fi
+    root=$(echo $(realpath "$(git rev-parse --git-common-dir)/.."))
+    if ! [[ -d "$root"/worktrees ]]; then
+        echo "no worktrees folder" >&2
+        return 1
+    fi
+    name="$1"
+    if [[ -d "$root"/worktrees/"$name" ]]; then
+        echo "worktree $name already exists"
+        return 1
+    fi
+    ref="$2"
+    [[ -z "$ref" ]] && ref="$(git_main_branch)"
+    wtpath="$root/worktrees/$name"
+    git worktree add "$wtpath" -b "collin/$name" "$ref"
+    cd "$wtpath"
+    if [[ -f "$root"/.claude ]]; then
+        ln -s "$root"/.claude .
+    fi
+    if [[ -f "$root"/mise.toml ]]; then
+        mise trust .
+        mise install
+    fi
 }
