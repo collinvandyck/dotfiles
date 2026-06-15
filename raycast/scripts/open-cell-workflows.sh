3391b9a72bdc2c3630c4625b7112fc282ee07a4a
Collin Van Dyck <collin@5xx.engineer>
2026-06-15 09:42:35 -0400

    Add cell workflows helpers.

---
+++ raycast/scripts/open-cell-workflows.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open Cell Workflows
+# @raycast.mode silent
+#
+# Optional parameters:
+# @raycast.packageName Temporal
+# @raycast.argument1 { "type": "text", "placeholder": "cell (e.g. s-aw028)", "optional": false }
+
+# Reads TEMPORAL_OPS_NAMESPACE from ~/.env
+if [ -f "$HOME/.env" ]; then . "$HOME/.env"; fi
+
+CELL="$1"
+: "${TEMPORAL_OPS_NAMESPACE:?set TEMPORAL_OPS_NAMESPACE in ~/.env}"
+open "https://cloud.temporal.io/namespaces/${TEMPORAL_OPS_NAMESPACE}/workflows?query=%60CellId%60%3D%22${CELL}%22"
