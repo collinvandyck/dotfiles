3094729c1c77a589875dbf325e1044d402752b22
Collin Van Dyck <collin@5xx.engineer>
2026-07-15 10:51:25 -0400

    Add open workflow run raycast helper script.

--- raycast/scripts/open-workflow-run.sh
+++ raycast/scripts/open-workflow-run.sh
@@ -2,28 +2,28 @@
 
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title Open Workflow
+# @raycast.title Open Workflow Run
 # @raycast.mode silent
 #
 # Optional parameters:
 # @raycast.packageName Temporal
 # @raycast.argument1 { "type": "dropdown", "placeholder": "namespace", "data": [{"title": "scaffold.infra", "value": "scaffold.infra"}, {"title": "prod.infra", "value": "prod.infra"}, {"title": "prod.cp", "value": "prod.cp"}, {"title": "test.cp", "value": "test.cp"}] }
-# @raycast.argument2 { "type": "text", "placeholder": "workflow id (defaults to clipboard)", "optional": true }
+# @raycast.argument2 { "type": "text", "placeholder": "run id (defaults to clipboard)", "optional": true }
 
 set -euo pipefail
 
 namespace="${1:-}"
-workflow_id="${2:-}"
+run_id="${2:-}"
 
-if [[ -z "$workflow_id" ]]; then
-	workflow_id="$(pbpaste | tr -d '\r\n')"
+if [[ -z "$run_id" ]]; then
+	run_id="$(pbpaste | tr -d '\r\n')"
 fi
 
-if [[ -z "$workflow_id" ]]; then
+if [[ -z "$run_id" ]]; then
 	echo "Workflow ID required"
 	exit 1
 fi
 
-encoded_workflow_id="$(python3 -c 'import sys, urllib.parse; print(urllib.parse.quote(sys.argv[1], safe=""))' "$workflow_id")"
+encoded_run_id="$(python3 -c 'import sys, urllib.parse; print(urllib.parse.quote(sys.argv[1], safe=""))' "$run_id")"
 
-open "https://cloud.temporal.io/namespaces/${namespace}/workflows/${encoded_workflow_id}"
+open "https://cloud.temporal.io/namespaces/${namespace}/workflows?query=%60RunId%60%3D%22${encoded_run_id}%22"
