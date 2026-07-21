d23861e8df2b6b10320ef69e23a55318837fb96b
Collin Van Dyck <collin@5xx.engineer>
2026-07-21 09:09:37 -0400

    Remove old worktrees func.

--- zsh/functions/worktrees.zsh
+++ zsh/functions/worktrees.zsh
@@ -6,36 +6,3 @@ wt() {
 	[[ -z "$dir" ]] && return
 	cd -- "$dir"
 }
-
-# OLD. create a new worktree. assumes that you're in the repo
-old-new-worktree() {
-	if ! git rev-parse --git-common-dir &>/dev/null; then
-		echo "not in a git repo" >&2
-		return 1
-	fi
-	root=$(echo $(realpath "$(git rev-parse --git-common-dir)/.."))
-	if ! [[ -d "$root"/worktrees ]]; then
-		echo "no worktrees folder" >&2
-		return 1
-	fi
-	name="$1"
-	if [[ -d "$root"/worktrees/"$name" ]]; then
-		echo "worktree $name already exists"
-		return 1
-	fi
-	ref="$2"
-	[[ -z "$ref" ]] && ref="$(git_main_branch)"
-	wtpath="$root/worktrees/$name"
-	git worktree add "$wtpath" -b "collin/$name" "$ref"
-	cd "$wtpath"
-	if [[ -d "$root"/.claude ]]; then
-		ln -s "$root"/.claude .
-	fi
-	if [[ -d "$root"/gen ]]; then
-		ln -s "$root"/gen .
-	fi
-	if [[ -f "$root"/mise.toml ]]; then
-		mise trust .
-		mise install
-	fi
-}
