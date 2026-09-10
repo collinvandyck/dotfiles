b61924d143b6f7db3fbae0b3d7ecb56788c8b60a
Collin Van Dyck <collin@5xx.engineer>
2026-09-10 11:38:44 -0400

    Add helper fn to find a particular pod.

--- zsh/temporal.zsh
+++ zsh/temporal.zsh
@@ -9,3 +9,12 @@ which-release() {
 	)
 	[[ -n "$out" ]] && echo "$out"
 }
+
+admintools-pod() {
+	cell=${1:-$CELL}
+	[[ -z "$cell" ]] && {
+		echo "no cell provided"
+		return 1
+	}
+	ctt kc --context $cell get po -n temporal -l app.kubernetes.io/component=admintools -o custom-columns=:metadata.name --no-headers
+}
