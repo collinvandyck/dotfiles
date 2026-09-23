0f23cdf37e15dfddad16b0e227f852c28a384671
Collin Van Dyck <collin@5xx.engineer>
2026-09-23 06:53:34 -0700

    Add cell ns overview helper.

---
+++ raycast/scripts/open-cell-ns-overview.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Cell Namespace Overview
+# @raycast.mode silent
+#
+# Optional parameters:
+# @raycast.packageName Temporal
+# @raycast.argument1 { "type": "text", "placeholder": "cell (blank = last)", "optional": true }
+
+. "$(dirname "${BASH_SOURCE[0]}")/common.sh"
+
+# Reads TEMPORAL_OPS_NAMESPACE from ~/.env
+if [ -f "$HOME/.env" ]; then . "$HOME/.env"; fi
+
+use_cell "$1"
+
+open "https://${GRAFANA_HOST}/d/namen2wqp9ufsp/temporal-cloud-namespace-overview?orgId=1&from=now-1h&to=now&timezone=utc&var-env=prod&var-cluster=${CELL}&var-datasource=af7fe237-211e-413e-9723-41a73886bcbb&var-clickhouse_datasource=aey7czk2sodtse&var-logs_datasource=e008932a-e9dc-4b7a-819f-68b662f3dc51&var-infra_db_name=Prod%2FInfraDB&var-infra_db_datasource=c8ea5458-e200-47da-aabc-eea36405a733&var-rate=\$__rate_interval&var-deployments=frontend&var-deployments=matching&var-deployments=history&var-quantile=0.99&var-namespaces=\$__all"
